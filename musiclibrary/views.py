4d96d2b47f0ac56da4e98383b9490c7db301b1b4
Kendall Morgan <kuyukip@gmail.com>
2022-07-03 14:43:09 -0500

    modify views file to include POST method

--- musiclibrary/views.py
+++ musiclibrary/views.py
@@ -1,13 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import MusicLibrarySerializer
 from .models import MusicLibrary
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def music_library_list(request):
-    musiclibrary = MusicLibrary.objects.all()
-
-    serializer = MusicLibrarySerializer(musiclibrary, many=True)
-
-    return Response(serializer.data)
+    if request.method == 'GET':
+        musiclibrary = MusicLibrary.objects.all()
+        serializer = MusicLibrarySerializer(musiclibrary, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = MusicLibrarySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
