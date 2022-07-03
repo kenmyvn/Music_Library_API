03bc06a1dbdbabf40a3b20db3f05487f8d5baaeb
Kendall Morgan <kuyukip@gmail.com>
2022-07-03 14:33:41 -0500

    create admin, add music to database

--- musiclibrary/views.py
+++ musiclibrary/views.py
@@ -1,8 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import MusicLibrarySerializer
+from .models import MusicLibrary
 
 
 @api_view(['GET'])
 def music_library_list(request):
+    musiclibrary = MusicLibrary.objects.all()
 
-    return Response('ok')
+    serializer = MusicLibrarySerializer(musiclibrary, many=True)
+
+    return Response(serializer.data)
