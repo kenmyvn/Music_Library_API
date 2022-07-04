3bce36e132c7c362c062eb541d2b08305dd015d4
Kendall Morgan <kuyukip@gmail.com>
2022-07-04 11:45:27 -0500

    create async function for get all songs to console log on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    getAllSongs()
+    console.log('Hello World')
+  }, []);
+
+
+  async function getAllSongs(){
+    const response = await axios.get('http://127.0.0.1:8000/music/');
+    console.log(response.data);
+    setSongs(response.data)
+  }
+
   return (
     <div>
-
+      
+      <button onClick={() => getAllSongs()}>Get All Songs</button>
     </div>
   );
 }
